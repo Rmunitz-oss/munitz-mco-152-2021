1272807eca603d5f1a96bf20cfab6be4ec201ca8
Rmunitz-oss <58051290+Rmunitz-oss@users.noreply.github.com>
2021-02-09 16:03:34 -0500

    Added Dictionary class and DictionaryTest class

---
+++ src/Dictionary.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Dictionary {
+    private ArrayList <String> dictionaryEntries = new ArrayList<>();
+
+    /**
+     * @param filePath of dictionary text file
+     */
+    public Dictionary(String filePath) {
+        this.dictionaryEntries = buildDictionary(filePath);
+    }
+
+    /**
+     * converts dictionary file to ArrayList of words
+     * @param filePath of dictionary file
+     * @return  list of dictionary entries
+     */
+    public ArrayList<String> buildDictionary(String filePath){
+        try(Scanner scanner = new Scanner(new File(filePath))){
+            while (scanner.hasNextLine()) {
+                dictionaryEntries.add(scanner.next());
+                scanner.nextLine();
+            }
+        }
+        catch (FileNotFoundException error){
+            System.out.println(error.getMessage());
+
+        }
+        return dictionaryEntries;
+    }
+
+    /**
+     * searches dictionary for desired word
+     * @param  wordToFind in dictionary
+     * @return true if found, false if not
+     */
+    public boolean findWord (String wordToFind){
+        for (String word : dictionaryEntries)
+        {
+            if (wordToFind.equals(word)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/DictionaryTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DictionaryTest {
+    @Test
+    public void findWord() {
+        String filePath = "C:/Users/roche/IdeaProjects/Word Search/src/dictionary.txt";
+        Dictionary file = new Dictionary(filePath);
+        boolean successfulSearch = file.findWord("ASH");
+        Assert.assertTrue(successfulSearch);
+        boolean failedSearch = file.findWord("XYZ");
+        Assert.assertFalse(failedSearch);
+    }
+}
