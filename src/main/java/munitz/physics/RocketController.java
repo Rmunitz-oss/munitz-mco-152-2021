ddaa717c942154cf098fa1127801ebe9bab9eb1b
Rmunitz-oss <58051290+Rmunitz-oss@users.noreply.github.com>
2021-02-28 17:27:49 -0500

    Added RocketControllerTest class.

--- src/main/java/munitz/physics/RocketController.java
+++ src/main/java/munitz/physics/RocketController.java
@@ -2,8 +2,8 @@ package munitz.physics;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
 
 public class RocketController {
     @FXML
@@ -13,11 +13,7 @@ public class RocketController {
     @FXML
     TextField secondsField;
     @FXML
-    Text xCoordinateText;
-    @FXML
-    Text yCoordinateText;
-    @FXML
-    Text flightTimeText;
+    Label locationLabel;
     @FXML
     RocketCanvas rocketCanvas;
 
@@ -25,12 +21,11 @@ public class RocketController {
         double velocity = Double.parseDouble(velocityField.getText());
         double angle = Double.parseDouble(angleField.getText());
         double seconds = Double.parseDouble(secondsField.getText());
-
         Rocket rocket = new Rocket(velocity, angle);
-        xCoordinateText.setText(String.valueOf(rocket.getX(seconds)));
-        yCoordinateText.setText(String.valueOf(rocket.getY(seconds)));
-        flightTimeText.setText(String.valueOf(rocket.getFlightTime()));
-
+        String rocketLocation = String.format("Rocket location: (%.2f, %.2f) ",
+                rocket.getX(seconds),
+                rocket.getY(seconds));
+        locationLabel.setText(rocketLocation);
         rocketCanvas.draw(rocket);
     }
 }
